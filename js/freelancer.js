3fe65ae375f1a8822178ef4a592bf9e1a74c9c24
Carefree4 <carefreefour@gmail.com>
2017-03-19 14:24:51 -0700

    added carosel and tons of other stuff

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,10 +1,41 @@
 // Freelancer Theme JavaScript
-
-(function($) {
+(function ($) {
     "use strict"; // Start of use strict
 
+
+
+    // Slick
+    $(document).on('ready', function () {
+        $(".center").slick({
+            centerMode: true,
+            centerPadding: '60px',
+            slidesToShow: 3,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            responsive: [{
+                    breakpoint: 768,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        });
+    });
+
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('.page-scroll a').bind('click', function(event) {
+    $('.page-scroll a').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
@@ -19,8 +50,8 @@
     });
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){ 
-            $('.navbar-toggle:visible').click();
+    $('.navbar-collapse ul li a').click(function () {
+        $('.navbar-toggle:visible').click();
     });
 
     // Offset for Main Navigation
@@ -31,12 +62,12 @@
     })
 
     // Floating label headings for the contact form
-    $(function() {
-        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+    $(function () {
+        $("body").on("input propertychange", ".floating-label-form-group", function (e) {
             $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-        }).on("focus", ".floating-label-form-group", function() {
+        }).on("focus", ".floating-label-form-group", function () {
             $(this).addClass("floating-label-form-group-with-focus");
-        }).on("blur", ".floating-label-form-group", function() {
+        }).on("blur", ".floating-label-form-group", function () {
             $(this).removeClass("floating-label-form-group-with-focus");
         });
     });
